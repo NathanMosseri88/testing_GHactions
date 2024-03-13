ae18df455001319f6d32139852bd718a05bf238e
jacob <jakeytawil18@gmail.com>
2024-03-13 17:02:43 -0400

    added flask app and requirements for testing

--- new_file.py
+++ new_file.py
@@ -1,2 +1,12 @@
-def updated_code():
-	return 'random BS'
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/test', methods=['GET'])
+def test_flask_server():
+	return 'hello from testing server'
+
+
+if __name__ == '__main__':
+	app.run(debug=True, port=3000)
+
