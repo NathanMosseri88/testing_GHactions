066af40a0990abdaf5dff11e16c04eb3235233cf
jacob <jakeytawil18@gmail.com>
2024-03-26 11:43:47 -0400

    testing writing file to server

--- through_ssh.bat
+++ through_ssh.bat
@@ -3,4 +3,5 @@ cd "C:\Users\SimpleToWork\Desktop"
 (
   echo This is the content of the text file.
   echo This file was created and placed here through SSH and GitHub Actions
-) > from_ssh.txt
+  echo If this file exists it means that GH actions was able to run the file in the context of the server and write the file to the server
+) > from_GHaction.txt
