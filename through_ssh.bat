05e5a1f35a2b7e0a37b996dec0c45e73fe9646ff
jacob <jakeytawil18@gmail.com>
2024-03-26 12:23:10 -0400

    open notepad through batch

--- through_ssh.bat
+++ through_ssh.bat
@@ -5,4 +5,4 @@ cd "C:\Users\SimpleToWork\Desktop"
   echo This file was created and placed here through SSH and GitHub Actions
   echo If this file exists it means that GH actions was able to run the file in the context of the server and write the file to the server
 ) > from_GHaction.txt
-notepad from_GHaction.txt
+start notepad from_GHaction.txt
