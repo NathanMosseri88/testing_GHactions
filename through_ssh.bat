2ede0f5a494846c929289ecae183f146ba4aaa40
jacob <jakeytawil18@gmail.com>
2024-03-26 11:36:07 -0400

    updated batch to wrie file

--- through_ssh.bat
+++ through_ssh.bat
@@ -1,5 +1,6 @@
 @echo off
+cd "C:\Users\SimpleToWork\Desktop"
 (
   echo This is the content of the text file.
   echo This file was created and placed here through SSH and GitHub Actions
-) > "C:\Users\SimpleToWork\Downloads"
+) > from_ssh.txt
